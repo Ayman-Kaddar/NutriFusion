5099c0fddecc4230515a30c47a9b0c40fde1efa1
Ayman Kaddar <71832197+Ayman-Kaddar@users.noreply.github.com>
2024-03-25 15:23:29 +0100

    Imagenes añadidas

--- components/molecules/card/recetaCard.jsx
+++ components/molecules/card/recetaCard.jsx
@@ -11,17 +11,21 @@ const RecetaCard = ({ receta }) => {
 
   return (
     <div className="bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105">
-      <img
-        src={receta.image}
-        alt={receta.alt}
-        className="w-full h-auto rounded-lg mb-4"
-      />
+      <div className="relative aspect-w-4 aspect-h-3">
+        <img
+          src={receta.image}
+          alt={receta.alt}
+          className="w-full h-64 object-cover rounded-t-lg"
+          loading="lazy"
+        />
+      </div>
       <div className="p-6">
         <h2 className="text-xl font-semibold text-primary-700 mb-2">
           {receta.nombre}
         </h2>
-        <p className="text-gray-600 mb-2">{receta.porciones}</p>
-        <p className="text-gray-600 mb-4">{receta.tiempo_preparacion}</p>
+        <p className="text-gray-600 mb-2">
+          Tiempo: {receta.tiempo_preparacion}
+        </p>
         <h3 className="text-lg font-semibold text-primary-700 mb-2">
           Ingredientes:
         </h3>
@@ -35,12 +39,18 @@ const RecetaCard = ({ receta }) => {
         <h3 className="text-lg font-semibold text-primary-700 mb-2">
           Instrucciones:
         </h3>
-        <ol className="list-decimal list-inside">
-          {receta.instrucciones.map((instruccion, i) => (
-            <li key={i} className="text-gray-600">
-              {instruccion}
-            </li>
-          ))}
+        <ol
+          className={`list-decimal list-inside ${
+            mostrarDescripcionCompleta ? "" : "truncate"
+          }`}
+        >
+          {receta.instrucciones
+            .slice(0, receta.instrucciones.length / 2)
+            .map((instruccion, i) => (
+              <li key={i} className="text-gray-600">
+                {instruccion}
+              </li>
+            ))}
         </ol>
         <div className="flex justify-center">
           <button
